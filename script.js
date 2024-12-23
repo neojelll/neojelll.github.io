1a01e23dc69a00eaf4e8064e915e4146d83f976b
deadonted <timofejmitusin60892@gmail.com>
2024-12-24 01:11:56 +0300

    test

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 let gamesData = [];
 
-        async function loadGames() {
-            const response = await fetch('psn_games.json');
-            gamesData = await response.json();
-            displayGames(gamesData);
-        }
+async function loadGames() {
+    const response = await fetch('psn_games.json');
+    gamesData = await response.json();
+    displayGames(gamesData);
+}
 
-        function displayGames(games) {
-            const gamesContainer = document.getElementById('games');
-            gamesContainer.innerHTML = '';
-            games.forEach(game => {
-                const gameDiv = document.createElement('div');
-                gameDiv.classList.add('game');
-                gameDiv.innerHTML = `
-                    <strong>Название:</strong> ${game.title}<br>
-                    <strong>Скидка:</strong> ${game.discount}<br>
-                    <strong>Дата окончания:</strong> ${game.discount_expire}<br>
-                    <a class="btn" href="${game.link}" target="_blank">Подробнее</a>
-                `;
-                gamesContainer.appendChild(gameDiv);
-            });
-        }
+function displayGames(games) {
+    const gamesContainer = document.getElementById('games');
+    gamesContainer.innerHTML = '';
+    games.forEach(game => {
+        const gameDiv = document.createElement('div');
+        gameDiv.classList.add('game');
+        gameDiv.innerHTML = `
+            <strong>Название:</strong> ${game.title}<br>
+            <strong>Скидка:</strong> ${game.discount}<br>
+            <strong>Дата окончания:</strong> ${game.discount_expire}<br>
+            <a class="btn" href="${game.link}" target="_blank">Подробнее</a>
+        `;
+        gamesContainer.appendChild(gameDiv);
+    });
+}
 
-        function filterGames() {
-            const minDiscount = parseInt(document.getElementById('minDiscount').value);
-            const filteredGames = gamesData.filter(game => {
-                // Извлекаем число из строки скидки
-                const discount = parseInt(game.discount.replace('%', ''));
-                return discount <= minDiscount;
-            });
-            displayGames(filteredGames);
-        }
+function filterGames() {
+    const minDiscount = parseInt(document.getElementById('minDiscount').value);
+    if (isNaN(minDiscount)) {
+        alert("Пожалуйста, введите корректное число для минимальной скидки.");
+        return;
+    }
+    
+    const filteredGames = gamesData.filter(game => {
+        // Извлекаем число из строки скидки
+        const discount = parseInt(game.discount.replace('%', ''));
+        return discount <= minDiscount;
+    });
+    
+    displayGames(filteredGames);
+}
 
-        // Инициализация загрузки игр
-        loadGames();
-		
+// Инициализация загрузки игр
+loadGames();
