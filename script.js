198b9565decabcdcacb0a6526b4c3a4639a6f37b
deadonted <timofejmitusin60892@gmail.com>
2024-12-24 01:48:23 +0300

    test

--- script.js
+++ script.js
@@ -13,12 +13,10 @@ function displayGames(games) {
         const gameDiv = document.createElement('div');
         gameDiv.classList.add('game');
         gameDiv.innerHTML = `
-            <div class="game">
-                <div class="discount">${game.discount}</div>
-                <h2>Название:</h2> ${game.title}<br>
-                <p>Дата окончания:</p> ${game.discount_expire}<br>
-                <a class="btn" href="${game.link}" target="_blank">Подробнее</a>
-            </div>
+            <strong class='text'>Название:</strong> ${game.title}<br>
+            <strong class='text'>Скидка:</strong> <span class="discount">${game.discount}</span><br>
+            <strong class='text'>Дата окончания:</strong> ${game.discount_expire}<br>
+            <a class="btn" href="${game.link}" target="_blank">Подробнее</a>
         `;
         gamesContainer.appendChild(gameDiv);
     });
